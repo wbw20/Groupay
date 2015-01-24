d90c92f1af02b8e8fe1e38fef40de974d2991318
Kris Sullivan <ksullivan@Kriss-MacBook-Pro.local>
2015-01-24 14:33:16 -0800

    static pages

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', function(request, response) {
   response.render('index', { title: 'The index page!' })
 });
 
+app.get('/room', function(request, response) {
+  response.render('room', { title: 'The room page!' })
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
